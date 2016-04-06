5340ea7bb8f0672a9951c4532e0cf96b55d136b6
olidi10 <mmutanga@yahoo.fr>
2016-04-06 18:21:18 -0500

    weather app home work

--- js/script.js
+++ js/script.js
@@ -1,6 +1,46 @@
-$.ready(function() {
-    
-   console.log("hello"); 
-    
-    
+$(function () {
+
+    $('form').on("submit", function (e) {
+        e.preventDefault();
+        var zipcode = $("#zip").val();
+        console.log(zipcode);
+        var apiKey = $("#apikey").val();
+        console.log(apiKey);
+        var url = "http://api.openweathermap.org/data/2.5/weather?zip=" + zipcode + ",us&appid=" + apiKey;
+        console.log(zipcode, apiKey, url)
+        $.getJSON(url)
+            .done(function (data) {
+
+                var weatherInfo = "";
+                weatherInfo += "<p> humidity " + data.main.humidity + "</p>";
+                weatherInfo += "<h2>temperature " + converTemp(data.main.temp) + "</p>";
+                weatherInfo += "<p>wind " + data.wind.speed + "</p>";
+                $("form").append("<div>" + weatherInfo + "</div>");
+
+
+            });
+
+
+
+
+
+
+
+
+
+    });
+
+    function converTemp(temp) {
+
+        return Math.round((temp * 1.8) - 459.67);
+    }
+
+
+
+
+
+
+
+
+
 });
